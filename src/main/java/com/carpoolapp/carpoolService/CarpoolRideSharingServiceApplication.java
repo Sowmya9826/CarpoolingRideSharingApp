e8a11c424167ad9ec4c617530c498fb28ff94cc9
Pujitha Kallu <pkallu@scu.edu>
2024-11-08 18:01:41 -0800

    intial steup

--- src/main/java/com/carpoolapp/carpoolService/CarpoolRideSharingServiceApplication.java
+++ src/main/java/com/carpoolapp/carpoolService/CarpoolRideSharingServiceApplication.java
@@ -2,8 +2,9 @@ package com.carpoolapp.carpoolService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class CarpoolRideSharingServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/carpoolapp/carpoolService/controller/UserController.java
+++ src/main/java/com/carpoolapp/carpoolService/controller/UserController.java
@@ -1,4 +1,17 @@
 package com.carpoolapp.carpoolService.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @GetMapping("/{id}")
+    public String getUserById(@PathVariable String id) {
+        // Return a simple string response for testing
+        return "User with ID " + id + " is a test user1.";
+    }
 }
