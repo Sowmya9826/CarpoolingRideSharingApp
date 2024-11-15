0d8878c25f4b88b2e7394f6ab6dbf2b783f340b5
Sowmya <sowmya@rohithapleWatch.localdomain>
2024-11-14 20:05:12 -0800

    Location Controller and repository updated Location controller updated Post create and Get Id method

--- src/main/java/com/carpoolapp/carpoolService/controller/LocationController.java
+++ src/main/java/com/carpoolapp/carpoolService/controller/LocationController.java
@@ -1,8 +1,44 @@
 package com.carpoolapp.carpoolService.controller;
-
+import com.carpoolapp.carpoolService.dto.LocationDto;
+import com.carpoolapp.carpoolService.models.Location;
+import com.carpoolapp.carpoolService.respository.LocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.Optional;
+@RestController
+@RequestMapping("/location")
 public class LocationController {
     /*
     location/create --POST
     location/{id}. --GET
      */
+    @Autowired
+    private LocationRepository locationRepository;
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createLocation(@RequestBody LocationDto locationDto) {
+        Location location = new Location();
+        location.setLatitude(locationDto.getLatitude());
+        location.setLongitude(locationDto.getLongitude());
+        location.setAddress(locationDto.getAddress());
+
+        locationRepository.save(location);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Location created successfully");
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<LocationDto> getLocationById(@PathVariable Long id) {
+        Optional<Location> location = locationRepository.findById(id);
+        if (location.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        } else {
+            LocationDto locationDto = new LocationDto();
+            locationDto.setLatitude(location.get().getLatitude());
+            locationDto.setLongitude(location.get().getLongitude());
+            locationDto.setAddress(location.get().getAddress());
+            return ResponseEntity.ok(locationDto);
+        }
+    }
 }
